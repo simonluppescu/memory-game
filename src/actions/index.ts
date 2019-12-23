38acfd4c53304be27316fbf4def03f36eb426e5c
Simon Luppescu <simon.luppescu@gmail.com>
2019-12-23 23:25:26 +0900

    Basic functionality to limit revealing two cards at a time and hide them after two have shown

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,10 @@
-import { RevealCardAction, ActionNames } from "../types/actionData";
+import { RevealCardAction, ActionNames, HideCardAction } from "../types/actionData";
 
 export const revealCard = (cardId: number): RevealCardAction => ({
   type: ActionNames.REVEAL_CARD,
   cardId
+});
+
+export const hideCards = (): HideCardAction => ({
+  type: ActionNames.HIDE_CARDS
 });
