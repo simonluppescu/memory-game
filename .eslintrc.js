75e6b8aa08f325ffe427b776c1b1f780362ec86c
Simon Luppescu <simon.luppescu@gmail.com>
2019-12-27 22:29:14 +0900

    Added rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-empty-interface": "off"
   },
 };
