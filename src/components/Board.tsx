ebddbb353d141bfe77c693ea20c70a9eef482bca
Simon Luppescu <simon.luppescu@gmail.com>
2019-12-28 23:52:24 +0900

    Fixed up algorithm for getting initial revealed cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,55 @@
 import React from "react";
-import DataProcessorService from "../services/dataProcessorService";
 
+import DataProcessorService from "../services/dataProcessorService";
 import CardContainer from "../containers/CardContainer";
 import GameStatsContainer from "../containers/GameStatsContainer";
 import CardTimerContainer from "../containers/CardTimerContainer";
 import { CardData } from "../types/goalItems";
+import { Language } from "../types/general";
 
 interface Props {
   setRevealedCards: (cards: Array<CardData>) => void;
 }
 
-const Board: React.FC<Props> = (props) => {
-  const dataProcessor = new DataProcessorService();
-  const { allItems, shuffledItems } = dataProcessor;
-  const initialReveals = new Map<number, CardData>();
-  for (let i = 0; i < allItems.length && initialReveals.size < 3; i++) {
-    const item = allItems[i];
-    if (initialReveals.has(item.matcherId)) continue;
+class Board extends React.Component<Props> {
+  static readonly NUM_INITIAL_REVEALS = 4;
 
-    initialReveals.set(item.matcherId, item);
+  private _getInitialReveals(shuffledItems: Array<CardData>): Array<CardData> {
+    const initialReveals = new Map<number, CardData>();
+    for (
+      let i = 0, currLang = shuffledItems[i].language;
+      i < shuffledItems.length && initialReveals.size < Board.NUM_INITIAL_REVEALS;
+      i++
+    ) {
+      const item = shuffledItems[i];
+
+      if (initialReveals.has(item.matcherId) || item.language !== currLang) continue;
+
+      initialReveals.set(item.matcherId, item);
+      currLang = currLang === Language.ENGLISH ? Language.JAPANESE : Language.ENGLISH;
+    }
+
+    return Array.from(initialReveals.values());
   }
 
-  props.setRevealedCards(Array.from(initialReveals.values()));
+  render() {
+    const dataProcessor = new DataProcessorService();
+    const { shuffledItems } = dataProcessor;
 
-  return (
-    <div className="board">
-      <GameStatsContainer />
-      <CardTimerContainer />
-      {shuffledItems.map((item, index) => (
-        <CardContainer key={index} itemData={item}></CardContainer>
-      ))}
-    </div>
-  );
-};
+    const initialReveals = this._getInitialReveals(shuffledItems);
+
+    this.props.setRevealedCards(initialReveals);
+
+    return (
+      <div className="board">
+        <GameStatsContainer />
+        <CardTimerContainer />
+        {shuffledItems.map((item, index) => (
+          <CardContainer key={index} itemData={item}></CardContainer>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Board;
